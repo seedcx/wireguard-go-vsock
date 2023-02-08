6d6814283868a0a87436bd5afeec6c8919fc1a6e
Guilherme Balena Versiani <guibv@mailbox.org>
2023-02-08 14:05:36 +0000

    Added support to `SOCK_STREAM` vsocks.

--- examples/http-client/main.go
+++ examples/http-client/main.go
@@ -20,7 +20,8 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
-	dev := device.NewDevice(tun, vsockconn.NewVsockSocketBind(), device.NewLogger(device.LogLevelVerbose, ""))
+	logger := device.NewLogger(device.LogLevelVerbose, "")
+	dev := device.NewDevice(tun, vsockconn.NewVsockStreamBind(logger), logger)
 	err = dev.IpcSet(`private_key=087ec6e14bbed210e7215cdc73468dfa23f080a1bfb8665b2fd809bd99d28379
 public_key=c4c8e984c5322c8184c72265b92b250fdb63688705f504ba003c88f03393cf28
 allowed_ip=0.0.0.0/0
