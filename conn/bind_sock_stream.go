10d5f7f7a51e3e8216b9af9ee002351319c71e29
Guilherme Balena Versiani <guilherme.versiani@zerohash.com>
2023-02-10 20:46:13 +0000

    Merged all implementations in one.

--- conn/bind_sock_stream.go
+++ conn/bind_sock_stream.go
@@ -1,46 +1,237 @@
 package vsockconn
 
 import (
+	"context"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"net/netip"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
-	"golang.org/x/sys/unix"
+	"github.com/jpillora/backoff"
+	"github.com/mdlayher/vsock"
 	"golang.zx2c4.com/wireguard/conn"
 	"golang.zx2c4.com/wireguard/device"
 )
 
-var (
-	_ conn.Bind = (*SocketStreamBind)(nil)
+const (
+	maxMTU              = 1500
+	ethernetMinimumSize = 64
+
+	defaultReconnectIntervalMin    = 500 * time.Millisecond
+	defaultReconnectIntervalMax    = 30 * time.Second
+	defaultReconnectIntervalFactor = 2
 )
+
+var (
+	_ conn.Bind     = (*SocketStreamBind)(nil)
+	_ conn.Endpoint = StreamEndpoint{}
+)
+
+type StreamEndpoint struct {
+	src net.Addr
+	dst net.Addr
+}
+
+func (e StreamEndpoint) ClearSrc() {
+	e.src = nil
+}
+
+func (e StreamEndpoint) DstIP() netip.Addr {
+	if e.dst != nil {
+		switch dst := e.dst.(type) {
+		case *net.TCPAddr:
+			return dst.AddrPort().Addr()
+		case *vsock.Addr:
+			return netip.AddrFrom4([4]byte{
+				byte(dst.ContextID >> 24),
+				byte(dst.ContextID >> 16),
+				byte(dst.ContextID >> 8),
+				byte(dst.ContextID),
+			})
+		}
+	}
+	return netip.Addr{}
+}
+
+func (e StreamEndpoint) SrcIP() netip.Addr {
+	if e.src != nil {
+		switch src := e.src.(type) {
+		case *net.TCPAddr:
+			return src.AddrPort().Addr()
+		case *vsock.Addr:
+			return netip.AddrFrom4([4]byte{
+				byte(src.ContextID >> 24),
+				byte(src.ContextID >> 16),
+				byte(src.ContextID >> 8),
+				byte(src.ContextID),
+			})
+		}
+	}
+	return netip.Addr{}
+}
+
+func (e StreamEndpoint) DstToBytes() []byte {
+	if e.dst != nil {
+		switch dst := e.dst.(type) {
+		case *net.TCPAddr:
+			return dst.AddrPort().Addr().AsSlice()
+		case *vsock.Addr:
+			return []byte{
+				byte(dst.ContextID >> 24),
+				byte(dst.ContextID >> 16),
+				byte(dst.ContextID >> 8),
+				byte(dst.ContextID),
+			}
+		}
+	}
+	return []byte{}
+}
+
+func (e StreamEndpoint) DstToString() string {
+	if e.dst != nil {
+		switch dst := e.dst.(type) {
+		case *net.TCPAddr:
+			return dst.AddrPort().Addr().String()
+		case *vsock.Addr:
+			parts := strings.Split(dst.String(), ":")
+			return parts[0]
+		}
+	}
+	return ""
+}
+
+func (e StreamEndpoint) SrcToString() string {
+	if e.src != nil {
+		switch src := e.src.(type) {
+		case *net.TCPAddr:
+			return src.AddrPort().Addr().String()
+		case *vsock.Addr:
+			parts := strings.Split(src.String(), ":")
+			return parts[0]
+		}
+	}
+	return ""
+}
+
+type Option func(bind *SocketStreamBind)
+
+func ReconnectIntervalMin(t time.Duration) Option {
+	return func(bind *SocketStreamBind) {
+		bind.b.Min = t
+	}
+}
+
+func ReconnectIntervalMax(t time.Duration) Option {
+	return func(bind *SocketStreamBind) {
+		bind.b.Max = t
+	}
+}
+
+func ReconnectIntervalFactor(factor float64) Option {
+	return func(bind *SocketStreamBind) {
+		bind.b.Factor = factor
+	}
+}
+
+func ReconnectIntervalJitter(b bool) Option {
+	return func(bind *SocketStreamBind) {
+		bind.b.Jitter = b
+	}
+}
+
+type streamDatagram struct {
+	b   []byte
+	src net.Addr
+}
 
 type SocketStreamBind struct {
 	log *device.Logger
-	mu  sync.RWMutex
-	l   *StreamListener
-	c   *StreamConn
+
+	wg     sync.WaitGroup
+	mu     sync.RWMutex
+	ctx    context.Context
+	cancel context.CancelFunc
+
+	l net.Listener
+
+	conns   map[net.Addr]net.Conn
+	network string
+
+	b       backoff.Backoff
+	dialers map[net.Addr]interface{}
+
+	received chan streamDatagram
 }
 
-func NewSocketStreamBind(logger *device.Logger) conn.Bind {
-	return &SocketStreamBind{log: logger}
+func NewSocketStreamBind(ctx context.Context, network string, logger *device.Logger, opts ...Option) conn.Bind {
+	ctx, cancel := context.WithCancel(ctx)
+
+	bind := &SocketStreamBind{
+		b: backoff.Backoff{
+			Min:    defaultReconnectIntervalMin,
+			Max:    defaultReconnectIntervalMax,
+			Factor: defaultReconnectIntervalFactor,
+			Jitter: true,
+		},
+		network: network,
+		log:     logger,
+		ctx:     ctx,
+		cancel:  cancel,
+	}
+
+	for _, opt := range opts {
+		opt(bind)
+	}
+
+	return bind
 }
 
 func (*SocketStreamBind) ParseEndpoint(s string) (conn.Endpoint, error) {
-	var end SockaddrInet4Endpoint
-	e, err := netip.ParseAddrPort(s)
-	if err != nil {
-		return nil, err
-	}
-
-	if e.Addr().Is4() {
-		dst := end.dst4()
-		dst.Port = int(e.Port())
-		dst.Addr = e.Addr().As4()
-		end.ClearSrc()
-		return &end, nil
+	if len(s) > 0 {
+		var end StreamEndpoint
+		if s[0] >= '0' && s[0] <= '9' {
+			e, err := netip.ParseAddrPort(s)
+			if err != nil {
+				return nil, err
+			}
+			end.dst = net.TCPAddrFromAddrPort(e)
+			return end, nil
+		} else {
+			for {
+				var err error
+				var contextID, port uint64
+				parts := strings.Split(s, ":")
+				if strings.HasPrefix(parts[0], "vm(") {
+					contextID, err = strconv.ParseUint(parts[0][3:len(parts[0])-1], 10, 32)
+				} else if strings.HasPrefix(parts[0], "host(") {
+					contextID, err = strconv.ParseUint(parts[0][5:len(parts[0])-1], 10, 32)
+				} else if strings.HasPrefix(parts[0], "local(") {
+					contextID, err = strconv.ParseUint(parts[0][6:len(parts[0])-1], 10, 32)
+				} else if strings.HasPrefix(parts[0], "hypervisor(") {
+					contextID, err = strconv.ParseUint(parts[0][11:len(parts[0])-1], 10, 32)
+				} else {
+					break
+				}
+				if err != nil {
+					break
+				}
+				port, err = strconv.ParseUint(parts[1], 10, 32)
+				if err != nil {
+					break
+				}
+				end.dst = &vsock.Addr{
+					ContextID: uint32(contextID),
+					Port:      uint32(port),
+				}
+				return end, nil
+			}
+		}
 	}
 
 	return nil, errors.New("invalid IP address")
@@ -50,16 +241,29 @@ func (bind *SocketStreamBind) Open(port uint16) ([]conn.ReceiveFunc, uint16, err
 	bind.mu.Lock()
 	defer bind.mu.Unlock()
 
-	if bind.l != nil || bind.c != nil {
+	if bind.l != nil {
 		return nil, 0, conn.ErrBindAlreadyOpen
 	}
 
 	if port != 0 {
-		l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		l, err := func() (net.Listener, error) {
+			switch bind.network {
+			case "tcp":
+				return net.Listen("tcp", fmt.Sprintf(":%d", port))
+			case "vsock":
+				return vsock.ListenContextID(AnyCID, uint32(port), nil)
+			default:
+				panic(net.UnknownNetworkError(bind.network))
+			}
+		}()
+
 		if err != nil {
 			return nil, 0, err
 		}
-		bind.l = NewStreamListener(l, bind.log)
+
+		bind.l = l
+		bind.wg.Add(1)
+		go bind.serve()
 	}
 
 	return []conn.ReceiveFunc{bind.makeReceiveFunc()}, port, nil
@@ -72,109 +276,197 @@ func (bind *SocketStreamBind) SetMark(value uint32) error {
 func (bind *SocketStreamBind) Close() error {
 	bind.mu.Lock()
 	defer bind.mu.Unlock()
+	bind.cancel()
 	if bind.l != nil {
 		bind.l.Close()
 	}
-	if bind.c != nil {
-		bind.c.Close()
-	}
+	bind.wg.Wait()
 	return nil
 }
 
 func (bind *SocketStreamBind) Send(buff []byte, end conn.Endpoint) error {
-	_, ok := end.(*SockaddrInet4Endpoint)
+	se, ok := end.(*StreamEndpoint)
 	if !ok {
 		return conn.ErrWrongEndpointType
 	}
 
-	bind.mu.RLock()
-	defer bind.mu.RUnlock()
-
-	if bind.l != nil {
-		return bind.l.Write(buff)
-	}
-
-	if bind.c == nil {
-		bind.mu.RUnlock()
-		err := bind.lockedConnect(end)
-		if err != nil {
-			return nil
+	found, err := bind.lockedSend(buff, se)
+	if !found {
+		bind.mu.Lock()
+		defer bind.mu.Unlock()
+		if _, ok := bind.dialers[se.dst]; !ok {
+			bind.dialers[se.dst] = true
+			bind.wg.Add(1)
+			go bind.dial(se.dst)
 		}
-		bind.mu.RLock()
+		return net.ErrClosed
 	}
 
-	err := bind.c.Write(buff)
-	if err != nil {
-		bind.lockedDisconnect()
-	}
 	return err
 }
 
-func (bind *SocketStreamBind) lockedConnect(end conn.Endpoint) error {
-	bind.mu.Lock()
-	defer bind.mu.Unlock()
+func (bind *SocketStreamBind) lockedSend(b []byte, se *StreamEndpoint) (bool, error) {
+	bind.mu.RLock()
+	defer bind.mu.RUnlock()
 
-	// Already connected
-	if bind.c != nil {
-		return nil
+	conn, ok := bind.conns[se.dst]
+	if !ok {
+		return false, nil
 	}
 
-	conn, err := net.Dial("tcp", end.DstToString())
+	// Write the packet's size.
+	err := binary.Write(conn, binary.LittleEndian, uint16(len(b)))
 	if err != nil {
-		return err
+		return ok, err
 	}
 
-	bind.c = NewStreamConn(conn, bind.log)
-	return nil
-}
-
-func (bind *SocketStreamBind) lockedDisconnect() {
-	bind.mu.Lock()
-	defer bind.mu.Unlock()
-
-	// Already disconnected
-	if bind.c == nil {
-		return
+	// Write as many bytes as required to send over the whole packet.
+	for len(b) > 0 {
+		n, err := conn.Write(b)
+		if err != nil {
+			return ok, err
+		}
+		b = b[n:]
 	}
 
-	bind.c.Close()
-	bind.c = nil
+	return ok, nil
 }
 
 func (bind *SocketStreamBind) makeReceiveFunc() conn.ReceiveFunc {
 	return func(b []byte) (int, conn.Endpoint, error) {
-		bind.mu.RLock()
-		defer bind.mu.RUnlock()
+		select {
+		case <-bind.ctx.Done():
+			return 0, nil, net.ErrClosed
+		case d := <-bind.received:
+			pktlen := len(d.b)
+			copy(b[:pktlen], d.b)
+			return pktlen, StreamEndpoint{src: d.src}, nil
+		}
+	}
+}
 
-	again:
-		n, rAddr, err := func() (int, net.Addr, error) {
-			for {
-				if bind.l != nil {
-					return bind.l.Read(b)
-				}
-				if bind.c != nil {
-					return bind.c.Read(b)
-				}
-				bind.mu.RUnlock()
-				time.Sleep(1 * time.Second)
-				bind.mu.RLock()
-			}
-		}()
+func (bind *SocketStreamBind) serve() {
+	bind.log.Verbosef("Routine: listener worker - started")
+	defer bind.log.Verbosef("Routine: listener worker - stopped")
+
+	defer bind.wg.Done()
+
+	for {
+		conn, err := bind.l.Accept()
 		if err != nil {
-			if errors.Is(err, net.ErrClosed) || errors.Is(err, unix.ECONNRESET) {
-				bind.mu.RUnlock()
-				time.Sleep(1 * time.Second)
-				bind.mu.RLock()
-				goto again
+			if _, ok := err.(*net.OpError); ok {
+				bind.log.Verbosef("Accept error: %w", err)
 			}
-			return 0, nil, err
+			return
+		}
+		bind.handleConn(conn, make(chan interface{}))
+	}
+}
+
+func (bind *SocketStreamBind) handleConn(conn net.Conn, reconnect chan<- interface{}) {
+	bind.mu.Lock()
+	defer bind.mu.Unlock()
+	bind.conns[conn.RemoteAddr()] = conn
+	bind.wg.Add(1)
+	go bind.read(conn, reconnect)
+}
+
+func (bind *SocketStreamBind) read(conn net.Conn, reconnect chan<- interface{}) {
+	bind.log.Verbosef("Routine: server worker - started")
+	defer bind.log.Verbosef("Routine: server worker - stopped")
+
+	defer func() {
+		bind.mu.Lock()
+		defer bind.mu.Unlock()
+		delete(bind.conns, conn.RemoteAddr())
+		conn.Close()
+		bind.wg.Done()
+	}()
+
+	for {
+		b := make([]byte, maxMTU+ethernetMinimumSize)
+		n, err := bind.readFrame(conn, b)
+		if err != nil {
+			select {
+			case <-bind.ctx.Done():
+				return
+			default:
+				reconnect <- true
+				return
+			}
 		}
 
-		end, _ := bind.ParseEndpoint(rAddr.String())
-		endInet4, _ := end.(*SockaddrInet4Endpoint)
-		*endInet4.Src4() = *endInet4.Dst4()
-		endInet4.ClearDst()
+		bind.received <- streamDatagram{b[:n], conn.RemoteAddr()}
+	}
+}
 
-		return n, endInet4, nil
+func (bind *SocketStreamBind) readFrame(conn net.Conn, b []byte) (int, error) {
+	// Read the incoming packet's size as a binary value.
+	n, err := io.ReadFull(conn, b[:2])
+	if err != nil {
+		return 0, err
+	}
+	if n != 2 {
+		return 0, io.EOF
+	}
+
+	// Decode the incoming packet's size from binary.
+	size := int(binary.LittleEndian.Uint16(b[:2]))
+	if size > len(b) {
+		bind.log.Errorf("Error: attempted to write frame > %d: %d", len(b), size)
+		conn.Close()
+		return 0, net.ErrClosed
+	}
+
+	n, err = io.ReadFull(conn, b[:size])
+	if err != nil {
+		return 0, err
+	}
+	if n != size {
+		return 0, io.EOF
+	}
+
+	return n, nil
+}
+
+func (bind *SocketStreamBind) dial(dst net.Addr) {
+	bind.log.Verbosef("Routine: dialer worker - started")
+	defer bind.log.Verbosef("Routine: dialer worker - stopped")
+
+	defer bind.wg.Done()
+
+	b := bind.b
+	reconnect := make(chan interface{})
+
+	for {
+		conn, err := func() (net.Conn, error) {
+			switch dst.Network() {
+			case "tcp":
+				var d net.Dialer
+				return d.DialContext(bind.ctx, "tcp", dst.String())
+			case "vsock":
+				vsockAddr, _ := dst.(*vsock.Addr)
+				return vsock.Dial(vsockAddr.ContextID, vsockAddr.Port, nil)
+			default:
+				panic(net.UnknownNetworkError(dst.Network()))
+			}
+		}()
+
+		if err != nil {
+			select {
+			case <-bind.ctx.Done():
+				return
+			case <-time.After(b.Duration()):
+				continue
+			}
+		}
+
+		bind.handleConn(conn, reconnect)
+		select {
+		case <-bind.ctx.Done():
+			return
+		case <-reconnect:
+			continue
+		}
 	}
 }
