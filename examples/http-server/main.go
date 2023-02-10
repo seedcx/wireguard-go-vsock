802d4f2b8267abbddfe71935a11f1f6eb73ca42b
Guilherme Balena Versiani <guilherme.versiani@zerohash.com>
2023-02-10 22:35:23 +0000

    Code fixes and verbose log improvements.

--- examples/http-server/main.go
+++ examples/http-server/main.go
@@ -28,7 +28,7 @@ func main() {
 		device.LogLevelVerbose,
 		fmt.Sprintf("(%s) ", interfaceName),
 	)
-	dev := device.NewDevice(tun, vsockconn.NewSocketStreamBind(logger), logger)
+	dev := device.NewDevice(tun, vsockconn.NewSocketStreamBind("tcp", logger), logger)
 	dev.IpcSet(`private_key=003ed5d73b55806c30de3f8a7bdab38af13539220533055e635690b8b87ad641
 listen_port=10000
 public_key=f928d4f6c1b86c12f2562c10b07c555c5c57fd00f59e90c8d8d88767271cbf7c
