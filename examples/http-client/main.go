878cf7601448f27ef3ee54e1b44e49cff4adb2ee
Guilherme Balena Versiani <guibv@mailbox.org>
2023-05-12 19:37:49 +0000

    Removed netstack usage from the examples.

--- examples/http-client/main.go
+++ examples/http-client/main.go
@@ -5,52 +5,52 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"net/netip"
 
 	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/tun/netstack"
+	"golang.zx2c4.com/wireguard/tun"
 
 	vsockconn "github.com/balena/wireguard-go-vsock/conn"
 )
 
 func main() {
-	tun, tnet, err := netstack.CreateNetTUN(
-		[]netip.Addr{netip.MustParseAddr("203.0.113.2")},
-		[]netip.Addr{},
-		1420,
-	)
+	network := "vsock"
+	interfaceName := "wg0"
+	tun, err := tun.CreateTUN(interfaceName, device.DefaultMTU)
 	if err != nil {
 		log.Panic(err)
 	}
-	interfaceName, _ := tun.Name()
+	realInterfaceName, err := tun.Name()
+	if err == nil {
+		interfaceName = realInterfaceName
+	}
 	logger := device.NewLogger(
 		device.LogLevelVerbose,
 		fmt.Sprintf("(%s) ", interfaceName),
 	)
-	dev := device.NewDevice(tun, vsockconn.NewSocketStreamBind("tcp", logger), logger)
+	bind := vsockconn.NewSocketStreamBind(network, logger)
+	dev := device.NewDevice(tun, bind, logger)
 	err = dev.IpcSet(`private_key=087ec6e14bbed210e7215cdc73468dfa23f080a1bfb8665b2fd809bd99d28379
 listen_port=10001
 public_key=c4c8e984c5322c8184c72265b92b250fdb63688705f504ba003c88f03393cf28
 allowed_ip=0.0.0.0/0
-endpoint=127.0.0.1:10000
+endpoint=host(2):10000
 `)
 	err = dev.Up()
 	if err != nil {
 		log.Panic(err)
 	}
 
-	client := http.Client{
-		Transport: &http.Transport{
-			DialContext: tnet.DialContext,
-		},
+	client := http.Client{}
+	for {
+		resp, err := client.Get("http://203.0.113.1/")
+		if err != nil {
+			log.Printf("HTTP Client error: %v", err)
+			continue
+		}
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			log.Panic(err)
+		}
+		log.Println(string(body))
 	}
-	resp, err := client.Get("http://203.0.113.1/")
-	if err != nil {
-		log.Panic(err)
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Panic(err)
-	}
-	log.Println(string(body))
 }
--- examples/http-server/main.go
+++ examples/http-server/main.go
@@ -4,47 +4,43 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net"
 	"net/http"
-	"net/netip"
 
 	"golang.zx2c4.com/wireguard/device"
-	"golang.zx2c4.com/wireguard/tun/netstack"
+	"golang.zx2c4.com/wireguard/tun"
 
 	vsockconn "github.com/balena/wireguard-go-vsock/conn"
 )
 
 func main() {
-	tun, tnet, err := netstack.CreateNetTUN(
-		[]netip.Addr{netip.MustParseAddr("203.0.113.1")},
-		[]netip.Addr{},
-		1420,
-	)
+	network := "vsock"
+	interfaceName := "wg0"
+	tun, err := tun.CreateTUN(interfaceName, device.DefaultMTU)
 	if err != nil {
 		log.Panic(err)
 	}
-	interfaceName, _ := tun.Name()
+	realInterfaceName, err := tun.Name()
+	if err == nil {
+		interfaceName = realInterfaceName
+	}
 	logger := device.NewLogger(
 		device.LogLevelVerbose,
 		fmt.Sprintf("(%s) ", interfaceName),
 	)
-	dev := device.NewDevice(tun, vsockconn.NewSocketStreamBind("tcp", logger), logger)
+	bind := vsockconn.NewSocketStreamBind(network, logger)
+	dev := device.NewDevice(tun, bind, logger)
 	dev.IpcSet(`private_key=003ed5d73b55806c30de3f8a7bdab38af13539220533055e635690b8b87ad641
 listen_port=10000
 public_key=f928d4f6c1b86c12f2562c10b07c555c5c57fd00f59e90c8d8d88767271cbf7c
 allowed_ip=0.0.0.0/0
-endpoint=127.0.0.1:10001
+endpoint=vm(6):10001
 `)
 	dev.Up()
-	listener, err := tnet.ListenTCP(&net.TCPAddr{Port: 80})
-	if err != nil {
-		log.Panicln(err)
-	}
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		log.Printf("> %s - %s - %s", request.RemoteAddr, request.URL.String(), request.UserAgent())
 		io.WriteString(writer, "Hello from userspace TCP!")
 	})
-	err = http.Serve(listener, nil)
+	err = http.ListenAndServe(":80", nil)
 	if err != nil {
 		log.Panicln(err)
 	}
